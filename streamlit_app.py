6fca662fb351ca8c7f00ca8757cb3e76e5f3f438
Naveen <106550196+naveen-6735@users.noreply.github.com>
2024-08-10 09:36:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,5 +60,8 @@ if ingredients_list:
                     session.sql(my_insert_stmt).collect()
                     
                     st.success('Your Smoothie is ordered!', icon="✅")
+
+session.close()
+
         
         
