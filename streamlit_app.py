1462a1366df0c2810599f776185fd0b6bc57b3e1
Naveen <106550196+naveen-6735@users.noreply.github.com>
2024-08-11 00:31:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ from snowflake.snowpark.functions import col
 from requests.exceptions import ConnectionError
 import pandas as pd
 
-
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 st.write(
